9d632be08b241dac70db4b755910176bbe96a4a8
jzhang78 <50782348+jzhang78@users.noreply.github.com>
2019-05-19 17:01:02 -0700

    Update calcudoku.py

--- calcudoku.py
+++ calcudoku.py
@@ -1,6 +1,43 @@
+#Project4 - Calcudoku
+#Name: Kitty Zhuang and Joshua Zhang
+#Instructor: Einakian
+#Section: 1
+
+import funcs
+def main():
+    numcages = int(input())
+    cage = []
+    for num in range(numcages):
+        cagevalue = input()
+        listcage = cagevalue.split()
+        cage.append(listcage)
+    listof0 = []
+    for x in range(25):
+        listof0.append(0)
+    while
+
+
 [2,4] = 14
 row = index // 5 = 2
 col = index % 5 = 4
 index = row * 5 + col
 
 index_new = col * 5 + row
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Run the unit tests.
+if __name__ == '__main__':
+    main()
+
+
