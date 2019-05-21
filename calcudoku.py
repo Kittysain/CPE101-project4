054e5b981f61a9ccadcfdcf328767ab659de5f5d
Kittysain <50782289+Kittysain@users.noreply.github.com>
2019-05-21 16:13:20 -0700

    Update calcudoku.py

--- calcudoku.py
+++ calcudoku.py
@@ -3,25 +3,50 @@
 #Instructor: Einakian
 #Section: 1
 
-import funcs
+from funcs import *
+
 def main():
-    numcages = int(input())
+    num_cages = int(input())
     cage = []
-    for num in range(numcages):
-        cagevalue = input()
-        listcage = cagevalue.split()
-        cage.append(listcage)
-    listof0 = []
-    for x in range(25):
-        listof0.append(0)
-    while
+    for num in range(num_cages):
+        cage_value = input()
+        list_cage = cage_value.split()
+        cage.append(list_cage)
+
+    grid = [0]*25
+
+    print(grid)
+    print(cage)
+
+    all_check = validate_all(grid,cage)
+    cage_check = validate_cages(grid,cage)
+    row_check = validate_rows(grid)
+
+    print (all_check)
+    print (cage_check)
 
 
 
+    while row_check == False:
 
+        i = 0
+        while i < 25:
+            if cage_check == True and grid[i] <= 5:
+                grid[i] += 1
+                if grid[i] > 5:
+                    grid[i] = 0
+                    i -= 1
+                cage_check = validate_cages(grid, cage)
+                i += 1
 
+            elif cage_check == False:
+                grid[i-1] = 0
+                i -= 2
+                cage_check = validate_cages(grid, cage)
 
+        print(grid)
 
+        row_check = validate_rows(grid)
 
 
 # Run the unit tests.
