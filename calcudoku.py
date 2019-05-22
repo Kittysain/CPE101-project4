2b75256d34bf1aaa55fa1c5c582f5a1d351cfafe
jzhang78 <50782348+jzhang78@users.noreply.github.com>
2019-05-21 18:15:49 -0700

    Update calcudoku.py

--- calcudoku.py
+++ calcudoku.py
@@ -6,51 +6,35 @@
 from funcs import *
 
 def main():
-    num_cages = int(input())
+    numcages = int(input())
     cage = []
-    for num in range(num_cages):
-        cage_value = input()
-        list_cage = cage_value.split()
-        cage.append(list_cage)
+    for num in range(numcages):
+        cagevalue = input()
+        listcage = cagevalue.split()
+        cage.append(listcage)
+    grid = 25 * [0]
+    cell = 0
+    while cell < 25:
+        grid[cell] += 1
+        if validate_all(grid, cage) == True:
+            cell += 1
+        elif grid[cell] > 5:
+            grid[cell] = 0
+            cell -= 1
 
-    grid = [0]*25
+    newgrid = ""
+    for rows in range(5):
+        for col in range(5):
+            index = (rows * 5) + col
+            numbers = grid[index]
+            newgrid += " ".join(str(numbers))
+        newgrid += "\n"
 
-    print(grid)
-    print(cage)
-
-    all_check = validate_all(grid,cage)
-    cage_check = validate_cages(grid,cage)
-    row_check = validate_rows(grid)
-
-    print (all_check)
-    print (cage_check)
+    print(newgrid)
 
 
-
-    while row_check == False:
-
-        i = 0
-        while i < 25:
-            if cage_check == True and grid[i] <= 5:
-                grid[i] += 1
-                if grid[i] > 5:
-                    grid[i] = 0
-                    i -= 1
-                cage_check = validate_cages(grid, cage)
-                i += 1
-
-            elif cage_check == False:
-                grid[i-1] = 0
-                i -= 2
-                cage_check = validate_cages(grid, cage)
-
-        print(grid)
-
-        row_check = validate_rows(grid)
 
 
 # Run the unit tests.
 if __name__ == '__main__':
     main()
-
-
